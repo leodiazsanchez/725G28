c5b0898c0958578376a8cf9e16a2c0c8c78969db
agnesmich <152631448+agnesmich@users.noreply.github.com>
2023-12-01 16:55:18 +0100

    Update Implementering.sql

--- Implementering.sql
+++ Implementering.sql
@@ -15,7 +15,7 @@ DROP TABLE IF EXISTS Users;
 CREATE TABLE Users(
 	user_id INT PRIMARY KEY,
 	first_name VARCHAR(20),
-	last_name VARCHAR(20),
+	last_name VARCHAR(30),
 	date_of_birth DATE,
 	email VARCHAR(50) UNIQUE,
 	phone_number VARCHAR(15) UNIQUE,
@@ -121,27 +121,27 @@ CREATE TABLE BorrowedSlidesRow (
 
 ---- to add values to our users table we insert it here underneath
 
-INSERT INTO Users (user_id, first_name, last_name, date_of_birth, email, phone_number, [address], registration_date)
+INSERT INTO Users (user_id, first_name, last_name, date_of_birth, email, phone_number, address, registration_date)
 VALUES
   (1, 'Erik', 'Larsson', '1985-03-12', 'erik.larsson@email.com', '0701234567', 'Gatan 1, Stockholm', '2023-01-01'),
-  (2, 'Anna', 'Andersson', '1990-05-18', 'anna.andersson@email.com', '0709876543', 'Vägen 2, Göteborg', '2023-02-01'),
-  (3, 'Oscar', 'Svensson', '1988-09-22', 'oscar.svensson@email.com', '0705551234', 'Backen 3, Malmö', '2023-03-01'),
-  (4, 'Emelie', 'Johansson', '1993-11-08', 'emelie.johansson@email.com', '0703334444', 'Höjden 4, Uppsala', '2023-04-01'),
-  (5, 'Gustav', 'Nilsson', '1982-07-14', 'gustav.nilsson@email.com', '0702221111', 'Strand 5, Linköping', '2023-05-01'),
-  (6, 'Sofia', 'Berg', '1998-02-20', 'sofia.berg@email.com', '0706667777', 'Skog 6, Örebro', '2023-06-01'),
-  (7, 'Henrik', 'Persson', '1987-12-05', 'henrik.persson@email.com', '0709998888', 'Berg 7, Västerås', '2023-07-01'),
-  (8, 'Amanda', 'Holm', '1995-04-30', 'amanda.holm@email.com', '0701112222', 'Äng 8, Jönköping', '2023-08-01'),
-  (9, 'Karl', 'Eriksson', '1980-10-17', 'karl.eriksson@email.com', '0704445555', 'Sjö 9, Norrköping', '2023-09-01'),
-  (10, 'Maja', 'Lindqvist', '1991-06-25', 'maja.lindqvist@email.com', '0707776666','Villevägen 4, Linköping','2023-02-03');
+  (2, 'Anna', 'Andersson', '1990-05-18', 'anna.andersson@email.com', '0709876543', 'VÃ¤gen 2, GÃ¶teborg', '2023-02-01'),
+  (3, 'Oscar', 'Svensson', '1988-09-22', 'oscar.svensson@email.com', '0705551234', 'Backen 3, MalmÃ¶', '2023-03-01'),
+  (4, 'Emelie', 'Johansson', '1993-11-08', 'emelie.johansson@email.com', '0703334444', 'HÃ¶jden 4, Uppsala', '2023-04-01'),
+  (5, 'Gustav', 'Nilsson', '1982-07-14', 'gustav.nilsson@email.com', '0702221111', 'Strand 5, LinkÃ¶ping', '2023-05-01'),
+  (6, 'Sofia', 'Berg', '1998-02-20', 'sofia.berg@email.com', '0706667777', 'Skog 6, Ã–rebro', '2023-06-01'),
+  (7, 'Henrik', 'Persson', '1987-12-05', 'henrik.persson@email.com', '0709998888', 'Berg 7, VÃ¤sterÃ¥s', '2023-07-01'),
+  (8, 'Amanda', 'Holm', '1995-04-30', 'amanda.holm@email.com', '0701112222', 'Ã„ng 8, JÃ¶nkÃ¶ping', '2023-08-01'),
+  (9, 'Karl', 'Eriksson', '1980-10-17', 'karl.eriksson@email.com', '0704445555', 'SjÃ¶ 9, NorrkÃ¶ping', '2023-09-01'),
+  (10, 'Maja', 'Lindqvist', '1991-06-25', 'maja.lindqvist@email.com', '0707776666','VillevÃ¤gen 4, LinkÃ¶ping','2023-02-03');
   -- to print we select the whole table
   SELECT * FROM Users
-  -- Lägger in värden för hyllorna 
+  -- LÃ¤gger in vÃ¤rden fÃ¶r hyllorna 
   INSERT INTO Shelves (shelf_id,row_count,[location])
   VALUES
-  (1, 20, 'Utställningssal A'),
+  (1, 20, 'UtstÃ¤llningssal A'),
   (2, 20, 'Arkivrum B'),
-  (3, 30, 'Föremålsgalleri'),
-  (4, 10, 'Historiska utställningen'),
+  (3, 30, 'FÃ¶remÃ¥lsgalleri'),
+  (4, 10, 'Historiska utstÃ¤llningen'),
   (5, 10, 'Teknikavdelningen');
 
 SELECT * FROM Shelves
@@ -165,3 +165,34 @@ VALUES
   (5, 5, 505, 6, 'Petra, Jordan');
 
 SELECT * FROM Digs
+
+INSERT INTO Artifacts (artifact_id,dig_id,shelf_id,row,description,date)
+VALUES 
+  (1, 1, 1, 3, 'Ancient pottery', '2023-01-15'),
+  (2, 2, 2, 1, 'Bronze figurine', '2023-02-28'),
+  (3, 3, 3, 2, 'Stone tools', '2023-03-12'),
+  (4, 4, 4, 4, 'Ceramic artifacts', '2023-04-20'),
+  (5, 5, 5, 3, 'Glass beads', '2023-05-05');
+  SELECT * FROM Artifacts
+
+  INSERT INTO Topics (topic_name)
+	VALUES
+	('Middle Est'),
+	('Anicent Greek'),
+	('CUBAN'),
+	('From Torrevija'),
+	('Viking Time')
+	SELECT * FROM Topics
+
+  INSERT INTO Slides (slide_id,shelf_id,dig_id,topic_name,row)
+  VALUES
+    (1, 1,1 , 'Middle est',	1),
+    (2, 1,1 , 'Middle est', 2),
+    (3, 1,1 , 'Middle est', 2),
+    (4, 1,1 , 'Middle est', 4),
+    (5, 1,1 , 'Middle est', 2)
+
+	SELECT * FROM Slides
+
+
+	
