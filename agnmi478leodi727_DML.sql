64cb55dc54123020625398e586c9718bbe6b45c3
Leo Diaz Sanchez <leodiazsanchez01@gmail.com>
2024-01-10 15:40:08 +0100

    Update agnmi478leodi727_DML.sql

--- agnmi478leodi727_DML.sql
+++ agnmi478leodi727_DML.sql
@@ -16,6 +16,18 @@ EXEC BorrowBook
     @user_id = 1,
     @book_id = 7;
 
+EXEC BorrowBook
+    @user_id = 6,
+    @book_id = 3;
+
+EXEC BorrowBook
+    @user_id = 4,
+    @book_id = 6;
+
+EXEC BorrowBook
+    @user_id = 3,
+    @book_id = 4;
+
 -- Example of borrowing and returnuing slides
 
 EXEC BorrowSlide
@@ -38,6 +50,14 @@ EXEC BorrowSlide
 	@user_id = 9,
 	@slide_id = 3;
 
+EXEC BorrowSlide
+	@user_id = 9,
+	@slide_id = 1;
+
+EXEC BorrowSlide
+	@user_id = 2,
+	@slide_id = 12;
+
 -- Example of borrowing and returnuing artifacts
 
 EXEC BorrowArtifact
@@ -56,31 +76,107 @@ EXEC ReturnArtifact
 	@user_id = 4,
 	@artifact_id = 2;
 
-SELECT * FROM Borrowed_slides ORDER BY topic_name;
---SELECT * FROM Borrowed_books;
---SELECT * FROM Shelves_with_books;
---SELECT * FROM User_Borrowing_Summary
-SELECT * FROM BorrowedArtifactsCol;
-SELECT * FROM BorrowedArtifactsRow;
 
-SELECT * FROM Artifact_Report;
+EXEC BorrowArtifact
+    @user_id = 1,
+    @artifact_id = 1;
 
 
-SELECT * FROM Borrowed_books;
+EXEC BorrowArtifact
+    @user_id = 2,
+    @artifact_id = 3;
 
---FRÅGOR--
-SELECT * FROM Users WHERE user_type = 'Employee' ORDER BY first_name,last_name;
 
-SELECT * FROM Slide_Catalog ORDER BY topic_name;
+EXEC BorrowArtifact
+    @user_id = 3,
+    @artifact_id = 4;
 
-/*SELECT COUNT (Slide_id) AS slide_count, topic_name
-FROM Slides
-GROUP BY topic_name
-ORDER BY COUNT(slide_id) DESC;*/
+	
+EXEC BorrowArtifact
+    @user_id = 4,
+    @artifact_id = 5;
 
-DELETE FROM Users WHERE user_id=1;
 
---SELECT * FROM Users;
---SELECT * FROM Former_users;
---SELECT * FROM Books;
-SELECT * FROM Borrowed_books;
+EXEC BorrowArtifact
+    @user_id = 2,
+    @artifact_id = 7;
+
+--Using views
+SELECT * FROM Borrowed_books 
+ORDER BY 
+	borrower_name;
+
+SELECT * FROM Artifact_Report 
+ORDER BY
+	dig_location;
+
+SELECT * FROM Slide_Catalog  
+WHERE 
+	shelf_location = 'Utställningssal A'
+ORDER BY 
+	topic_name;
+
+SELECT * FROM Borrowed_slides 
+ORDER BY 
+	topic_name;
+
+--Simple queries--
+
+--Query to retrive employees from the users table
+SELECT * FROM Users	
+WHERE 
+	user_type = 'Employee' 
+ORDER BY 
+	first_name,last_name;
+
+-- Complex queries
+
+-- Query to retrieve the books borrowed within a specified date range
+SELECT 
+	B.title, BBC.borrow_date, BBC.return_date
+FROM 
+	Books B
+INNER JOIN 
+	BorrowedBooksRow BBR ON B.book_id = BBR.book_id
+INNER JOIN 
+	BorrowedBooksCol BBC ON BBR.order_id = BBC.order_id
+WHERE 
+	BBC.borrow_date BETWEEN '2023-01-01' AND '2024-12-31'
+ORDER BY
+	BBC.borrow_date DESC;
+
+-- Query to show how many artifacts each user has borrowed
+SELECT 
+	U.user_id, CONCAT(u.first_name, ' ', u.last_name) AS name, COUNT(BA.artifact_id) AS total_borrowed
+FROM 
+	BorrowedArtifactsRow BA
+INNER JOIN 
+	BorrowedArtifactsCol BAC ON BA.order_id = BAC.order_id
+INNER JOIN 
+	Users U ON BAC.user_id = U.user_id
+GROUP BY 
+	U.user_id, CONCAT(u.first_name, ' ', u.last_name)
+ORDER BY 
+	total_borrowed DESC;
+
+--Query to filter out the users that borrowed a ceratin number of books
+SELECT
+    Users.user_id,
+    Users.first_name,
+    Users.last_name,
+    COUNT(BorrowedBooksCol.order_id) AS num_borrowed_books
+FROM
+    Users
+LEFT JOIN 
+	BorrowedBooksCol ON Users.user_id = BorrowedBooksCol.user_id
+GROUP BY
+    Users.user_id, Users.first_name, Users.last_name
+HAVING
+    COUNT(BorrowedBooksCol.order_id) > 1; -- desired threshold
+
+-- Delete and verifying that trigger works as intended
+DELETE FROM Users 
+WHERE user_id = 1;
+
+SELECT * FROM Users;
+SELECT * FROM Former_users;
